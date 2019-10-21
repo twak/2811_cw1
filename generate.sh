728b531e8a9ec8d60d8df913b77ceef1f4a1bb2e
twak <twakelly@gmail.com>
2019-10-21 21:24:07 +0100

    generate script

--- generate.sh
+++ generate.sh
@@ -4,15 +4,17 @@ rm -rf ./report/images
 mkdir -p report
 echo "<html><body>" >> report/index.html
 echo "<h4>cmake output</h4><pre>" >> ./report/index.html
-cmake . >> ./report/index.html
+cmake . &>> ./report/index.html
+make clean &>> ./report/index.html
 echo "</pre>" >> ./report/index.html
 echo "<h4>make output</h4><pre>" >> ./report/index.html
-make >> ./report/index.html
+make &>> ./report/index.html
 echo "</pre>" >> ./report/index.html
 echo "<h4>program output</h4><pre>" >> ./report/index.html
 ./cw1 1  >> ./report/index.html
 echo "</pre>" >> ./report/index.html
-zip -r ../generate.zip .
-dir=$(pwd)
-parentdir="$(dirname "$dir")"
-echo "submit $parentdir/generate.zip"
+dir="$(pwd)"
+echo "$dir"
+parentzip="$(dirname $dir)/generate_$(whoami).zip"
+zip -r "$parentzip" .
+echo "submit $parentzip"
